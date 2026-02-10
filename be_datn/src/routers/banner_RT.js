6946e69231865fbbeee26be40a2d148e3ee6e03c
khanhvd1012 <khanhvdph49723@gmail.com>
2026-02-10 22:59:56 +0700

    add checkpersion banner

--- be_datn/src/routers/banner_RT.js
+++ be_datn/src/routers/banner_RT.js
@@ -2,14 +2,14 @@ import { Router } from "express";
 import { createBanner, deleteBanner, getAllBanners, getBannerById, toggleBannerStatus, updateBanner } from "../controllers/banner_CTL.js";
 import { validateBanner } from "../validators/banner_VLD.js";
 import upload from "../middleware/upload_MID.js";
-
+import { checkPermission } from "../middleware/checkPermission.js";
 const bannerRouter = Router();
 
 bannerRouter.get("/", getAllBanners);
 bannerRouter.get("/:id", getBannerById);
-bannerRouter.post("/", upload.single("image"), validateBanner, createBanner);
-bannerRouter.put("/:id", upload.single("image"), validateBanner, updateBanner);
-bannerRouter.delete("/:id", deleteBanner);
-bannerRouter.put("/:id/toggle-status", toggleBannerStatus);
+bannerRouter.post("/", upload.single("image"),checkPermission, validateBanner, createBanner);
+bannerRouter.put("/:id", upload.single("image"),checkPermission, validateBanner, updateBanner);
+bannerRouter.delete("/:id",checkPermission , deleteBanner);
+bannerRouter.put("/:id/toggle-status",checkPermission, toggleBannerStatus);
 
 export default bannerRouter;
